1633e42520ff90362dc78c0bd692e7bd7c1c20a3
Ricardo Yuri <ricardo-yuri@outlook.com>
2021-11-08 22:28:47 -0300

    First commit
    Adicionando Swagger Config

---
+++ src/main/java/com/br/panacademy/devcompilers/bluebank/BluebankApplication.java
@@ -0,0 +1,13 @@
+package com.br.panacademy.devcompilers.bluebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluebankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluebankApplication.class, args);
+	}
+
+}
